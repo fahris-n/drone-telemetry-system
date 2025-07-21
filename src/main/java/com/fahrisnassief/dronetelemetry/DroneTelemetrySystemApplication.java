7a1f62eb589fa73632230832ab84c0d99ef09f64
fahrisnassief <fahris0310@gmail.com>
2025-07-20 21:11:49 -0600

    Initial Commit: Setup Spring Boot project with basic config

---
+++ src/main/java/com/fahrisnassief/dronetelemetry/DroneTelemetrySystemApplication.java
@@ -0,0 +1,13 @@
+package com.fahrisnassief.dronetelemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneTelemetrySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneTelemetrySystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fahrisnassief/dronetelemetry/DroneTelemetrySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahrisnassief.dronetelemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneTelemetrySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
